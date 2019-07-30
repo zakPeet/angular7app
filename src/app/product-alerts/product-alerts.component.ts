cfb40997586db3f7f7a7dff2a094a04a1769db91
zakPeet <zaknduiga97@gmail.com>
2019-07-30 09:52:33 +0300

    1 file modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core'; //setting it up to receive input.
-import { Output,EventEmitter } from '@angular/core'
+import { Output,EventEmitter } from '@angular/core'; //will be producing an output
 
 @Component({
   selector: 'app-product-alerts',
@@ -9,6 +9,7 @@ import { Output,EventEmitter } from '@angular/core'
 })
 export class ProductAlertsComponent implements OnInit {
    @Input() product;  // recieved input prepared for above in imports
+  @Output() notify = new EventEmitter;  // for producing output.
   constructor() { }
 
   ngOnInit() {
